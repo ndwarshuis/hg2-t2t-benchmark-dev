80870b683f36e18695ef854ad83e3d8d5c302167
ndwarshuis <ndwar@yavin4.ch>
2023-09-20 11:06:43 -0400

    WIP use umap to analyze VDJ regions

--- pipelines/vdj_read_align/workflow/scripts/analyze_IGH.R
+++ pipelines/vdj_read_align/workflow/scripts/analyze_IGH.R
@@ -1,44 +1,179 @@
 library(tidyverse)
+library(umap)
+library(ggpubr)
 
 df <- read_tsv(
   "../../results/immuno_alignments/ont/split/IGH/final/allele.tsv"
 ) %>%
-  mutate(gene = sprintf("%s%s", major, minor))
+  mutate(gene = sprintf("%s%s*%s", major, minor, allele))
 
-v_df <- df %>%
-  filter(major == "V") %>%
-  group_by(rname) %>%
-  filter(max(gene_pos) == gene_pos) %>%
-  select(rname, gene) %>%
-  rename(V = gene)
+subset_major <- function(df, x, usemin = FALSE) {
+  .index_col <- paste0(x, "index")
+  .pos_col <- paste0(x, "pos")
+  .allele_col <- paste0(x, "allele")
+  .f <- if (usemin) min else max
+  df %>%
+    group_by(gene) %>%
+    mutate(allele = as.integer(factor(allele))) %>%
+    ungroup() %>%
+    filter(major == x) %>%
+    group_by(rname) %>%
+    filter(.f(pos) == pos) %>%
+    ungroup() %>%
+    select(rname, gene, gene_pos, pos, allele) %>%
+    rename(
+      !! x := gene,
+      !! .index_col := gene_pos,
+      !! .pos_col := pos,
+      !! .allele_col := allele
+    )
+}
 
-d_df <- df %>%
-  filter(major == "D") %>%
-  group_by(rname) %>%
-  filter(max(gene_pos) == gene_pos) %>%
-  select(rname, gene) %>%
-  rename(D = gene)
+v_df <- subset_major(df, "V")
+dmin_df <- subset_major(df, "D", TRUE) %>%
+  rename_with(~ paste0(.x, "_min"), starts_with("D"))
+dmax_df <- subset_major(df, "D") %>%
+  rename_with(~ paste0(.x, "_max"), starts_with("D"))
+j_df <- subset_major(df, "J", TRUE)
+c_df <- subset_major(df, "C", TRUE)
 
-j_df <- df %>%
-  filter(major == "J") %>%
-  group_by(rname) %>%
-  filter(min(gene_pos) == gene_pos) %>%
-  select(rname, gene) %>%
-  rename(J = gene)
+all_vdjc_df <- v_df %>%
+  full_join(dmin_df, by = "rname") %>%
+  full_join(dmax_df, by = "rname") %>%
+  full_join(j_df, by = "rname") %>%
+  full_join(c_df, by = "rname") %>%
+  mutate(
+    combo = str_c(V, J, C, sep = "_"),
+    Vindex = 159 - Vindex,
+    Dlen = Dindex_max - Dindex_min,
+    Dindex = Dindex_min - 160,
+    Jindex = Jindex - 186,
+    Cindex = Cindex - 196,
+    ## VDmin_dist = Dpos_min - Vpos,
+    ## VDmax_dist = Dpos_max - Vpos,
+    ## VJ_dist = Jpos - Vpos,
+    ## VC_dist = Cpos - Vpos,
+    ## DminDmax_dist = Dpos_max - Dpos_min,
+    ## DminJ_dist = Jpos - Dpos_min,
+    ## DminC_dist = Cpos - Dpos_min,
+    ## DmaxJ_dist = Jpos - Dpos_max,
+    ## DmaxC_dist = Cpos - Dpos_max,
+    ## JC_dist = Cpos - Jpos
+  ) %>%
+  select(-Dindex_min, -Dindex_max) %>%
+  replace_na(list(Dlen = 0, D_min = "UNK", D_max = "UNK")) %>%
+  mutate(across(matches("^(V|D|J|C).+", ignore.case = F), ~ replace_na(.x, -1)))
 
-vjc_df <- df %>%
-  filter(major == "C") %>%
-  group_by(rname) %>%
-  filter(min(gene_pos) == gene_pos) %>%
-  select(rname, gene) %>%
-  rename(C = gene) %>%
-  inner_join(v_df, by = "rname") %>%
-  inner_join(j_df, by = "rname") %>%
-  mutate(combo = str_c(V, J, C, sep = "_"))
+vdjc_df <- all_vdjc_df %>%
+  filter(Vindex > -1 & Cindex > -1)
 
-vjc_df %>%
-  ggplot(aes(y = combo)) +
-  geom_bar()
+jc_df <- all_vdjc_df %>%
+  anti_join(vdjc_df, by = "rname") %>%
+  filter(Jindex > -1 & Cindex > -1)
+
+vj_df <- all_vdjc_df %>%
+  anti_join(vdjc_df, by = "rname") %>%
+  anti_join(jc_df, by = "rname") %>%
+  filter(Vindex > -1 & Jindex > -1)
+
+vd_df <- all_vdjc_df %>%
+  anti_join(vdjc_df, by = "rname") %>%
+  anti_join(jc_df, by = "rname") %>%
+  anti_join(vj_df, by = "rname") %>%
+  filter(Vindex > -1 & Dlen > 0)
+
+dj_df <- all_vdjc_df %>%
+  anti_join(vdjc_df, by = "rname") %>%
+  anti_join(jc_df, by = "rname") %>%
+  anti_join(vj_df, by = "rname") %>%
+  anti_join(vd_df, by = "rname") %>%
+  filter(Dlen > 0 & Jindex > -1)
+
+v_only_df <- all_vdjc_df %>%
+  anti_join(vdjc_df, by = "rname") %>%
+  anti_join(jc_df, by = "rname") %>%
+  anti_join(vj_df, by = "rname") %>%
+  anti_join(vd_df, by = "rname") %>%
+  anti_join(dj_df, by = "rname") %>%
+  filter(Vindex > -1)
+
+c_only_df <- all_vdjc_df %>%
+  anti_join(vdjc_df, by = "rname") %>%
+  anti_join(jc_df, by = "rname") %>%
+  anti_join(vj_df, by = "rname") %>%
+  anti_join(vd_df, by = "rname") %>%
+  anti_join(dj_df, by = "rname") %>%
+  anti_join(v_only_df, by = "rname") %>%
+  filter(Cindex > -1)
+
+remainder <- all_vdjc_df %>%
+  anti_join(vdjc_df, by = "rname") %>%
+  anti_join(jc_df, by = "rname") %>%
+  anti_join(vj_df, by = "rname") %>%
+  anti_join(vd_df, by = "rname") %>%
+  anti_join(dj_df, by = "rname") %>%
+  anti_join(v_only_df, by = "rname") %>%
+  anti_join(c_only_df, by = "rname")
+
+group_list <- function(df) {
+  group_map(df, ~ .x) %>% set_names(unlist(group_keys(df)))
+}
+
+plot_umap_alleles <- function(df) {
+  umap_out <- df %>%
+    select(matches("index"), matches("allele"), Dlen) %>%
+    select(where(~ any(.x > -1))) %>%
+    umap()
+  .df <- umap_out$layout %>%
+    as_tibble(.name_repair = "minimal") %>%
+    set_names(c("V1", "V2")) %>%
+    bind_cols(df) %>%
+    select(V1, V2, rname, V, D_min, D_max, J, C) %>%
+    select(where(~ all(!is.na(.x))))
+  .df %>%
+    pivot_longer(cols = -c(rname, V1, V2), names_to = "major", values_to = "allele") %>%
+    group_by(major) %>%
+    group_list() %>%
+    imap(~ ggplot(.x, aes(V1, V2, color = allele)) +
+           geom_point() +
+           labs(x = NULL, y = NULL, color = paste0(.y, " allele"))) %>%
+    ggarrange(plotlist = .)
+}
+
+plot_umap_alleles(vdjc_df)
+
+plot_umap_alleles(jc_df)
+
+df %>%
+  right_join(vd_df, by = "rname") %>%
+  ggplot(aes(pos, gene_pos, group = rname, color = major)) +
+  geom_point() +
+  geom_line()
+
+df %>%
+  right_join(vj_df, by = "rname") %>%
+  ggplot(aes(pos, gene_pos, group = rname, color = major)) +
+  geom_point() +
+  geom_line()
+
+df %>%
+  right_join(dj_df, by = "rname") %>%
+  ggplot(aes(pos, gene_pos, group = rname, color = major)) +
+  geom_point() +
+  geom_line()
+
+df %>%
+  right_join(v_only_df, by = "rname") %>%
+  ggplot(aes(pos, gene_pos, group = rname, color = major)) +
+  geom_point() +
+  geom_line()
+
+df %>%
+  right_join(c_only_df, by = "rname") %>%
+  ggplot(aes(pos, gene_pos, group = rname, color = major)) +
+  geom_point() +
+  geom_line()
+
 
 # investigate the non-M clone(s) further
 nonM_df <- vjc_df %>%
