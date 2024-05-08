33d502be9dd438b1878e1da0a0e4f44897afc8b8
Nate Dwarshuis <njd2nist.gov>
2024-05-08 13:55:51 -0400

    ADD pav runtime parameters

---
+++ pipelines/hg2-pav/grch38/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem-per-cpu=3G
+#SBATCH --partition=batch
+#SBATCH --time=36:0:0
+
+singularity run --bind "$(pwd):/mnt" --pwd /mnt library://becklab/pav/pav:2.3.4 -c 32
